9d6d31f36f0a63f650f01f0d8a8798b53c465621
Carlos Alberto R <carlosalbertorios3@gmail.com>
2022-04-24 20:44:46 -0500

    The Button component was added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
       <div className='header__wrap container'>
         <div className='logo'>
           <img src={logo} alt='' />
-          <Link to='/'>MyMovies</Link>
+          <Link to='/'>My Movies</Link>
         </div>
         <ul className='header__nav'>
           {headerNav.map((e, i) => (
