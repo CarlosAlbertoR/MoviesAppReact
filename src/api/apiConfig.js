aedc206ed9b61916bf74d089dead693f4230d4c1
Carlos Alberto R <carlosalbertorios3@gmail.com>
2023-03-16 08:34:41 -0500

    👌 IMPROVE: cleanup

--- src/api/apiConfig.js
+++
@@ -1,8 +0,0 @@
-const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'a8b4257e96d4238a1d9b8379263933a1',
-    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
-}
-
-export default apiConfig;
