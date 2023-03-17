ff4be4f8778c88c7386e0264c69e8be26bb81a0f
Carlos Alberto R <carlosalbertorios3@gmail.com>
2023-03-16 21:53:04 -0500

    🐛 FIX: API calls

--- src/components/movie-list/MovieList.jsx
+++ src/components/movie-list/MovieList.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { SwiperSlide, Swiper } from 'swiper/react';
-import tmdbApi, { category } from '../../api/tmdbApi';
-import MovieCard from '../movie-card/MovieCard';
-import './MovieList.scss';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { SwiperSlide, Swiper } from "swiper/react";
+import tmdbApi, { category } from "../../api/tmdbApi";
+import MovieCard from "../movie-card/MovieCard";
+import "./MovieList.scss";
 
 const MovieList = (props) => {
   const [items, setItems] = useState([]);
@@ -13,7 +13,7 @@ const MovieList = (props) => {
       let response = null;
       const params = {};
 
-      if (props.type !== 'similar') {
+      if (props.type !== "similar") {
         switch (props.category) {
           case category.movie:
             response = await tmdbApi.getMoviesList(props.type, { params });
@@ -27,11 +27,11 @@ const MovieList = (props) => {
       setItems(response.results);
     };
     getList();
-  });
+  }, [props.category, props.id, props.type]);
 
   return (
-    <div className='movie-list'>
-      <Swiper grabCursor={true} spaceBetween={10} slidesPerView={'auto'}>
+    <div className="movie-list">
+      <Swiper grabCursor={true} spaceBetween={10} slidesPerView={"auto"}>
         {items.map((item, i) => (
           <SwiperSlide key={i}>
             <MovieCard item={item} category={props.category} />
